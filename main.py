3bc5e9514ef28ecfa0d9422b3f6044c45e63c497
Graham Holtslander <menello@gmail.com>
2012-09-24 00:27:05 -0600

    Now comes custom!
    If you run main.py you can customize your bot, as well as giving it a
    different server and port! Warning, this is completely untested :)

--- main.py
+++ main.py
@@ -3,18 +3,33 @@
 from ircbot import IrcBot
 
 # Set up your connection
-server = "irc.utonet.org"
-port = 6667
-buffer_size = 2048
+server = raw_input("Enter the name of your server: ")
+port = raw_input("Which port to use? (most commonly it is 6667, leave this blank to use that) ")
+if port is "":
+    port = 6667
+buffer_size = raw_input("What size of buffer do you want to take from the socket? 2048 or 4096 are most common, leave blank for 4096: ")
+if buffer_size is "":
+    buffer_size = 4096
 
-# Set up your bot
-nick = "LonelyBot"
-name = "Lonely Island Bot"
+# Personalize your bot
+nick = raw_input("What is your bot's name? ")
+name = raw_input("What is its full name? ")
 # this is important because you don't want any random to be sending some of the commands to the bot
 # this is a list of names
-admin_nicks = ["Tenotitwan", "TenotitwanWork"]
+print "Name some administrators (type 'done' to finish):"
+admin_add = raw_input("> ")
+admin_nicks = []
+while admin_add is not "done":
+    admin_nicks.append(admin_add)
+    admin_add = raw_input("Alright, any more?\n>")
+
 # a list of chat rooms to join
-chat_rooms = ["#LonelyIsland"]
+print "Name some chat rooms to join (including #; type 'done' to finish):"
+room_add = raw_input("> ")
+chat_rooms = []
+while room_add is not "done":
+    chat_rooms.append(room_add)
+    room_add = raw_input("Alright, any more?\n>")
 
 # Create new instance of bot, connect to designated server, grab some data
 bot = IrcBot(server, port, nick, name, admin_nicks)
